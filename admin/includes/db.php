0c78a82530ed488ddb203e38d171bd842e88c9ff
samuel addisu <samueladdisu7@gmail.com>
2022-02-16 15:21:35 +0300

    last attempt of db config2

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,7 +1,7 @@
 <?php
 ;
 
-include (basename(dirname(dirname(__FILE__))) . "/admin_config.php");
+include (basename(dirname(dirname(dirname(__FILE__)))) . "/admin_config.php");
 
 $db_username = $_ENV['DB_USERNAME'];
 $db_pwd = $_ENV['DB_PASSWORD'];
