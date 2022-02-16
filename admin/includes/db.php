79658ba5996ecd9270eba6fe08c67a09367c2e3c
samuel addisu <samueladdisu7@gmail.com>
2022-02-16 15:04:13 +0300

    admin config 3

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,7 +1,7 @@
 <?php
 
 
-include 'https://versavvymedia.com/reservation_system/admin_config.php';
+include ($_SERVER["DOCUMENT_ROOT"] . "/admin_config.php");
 
 $db_username = $_ENV['DB_USERNAME'];
 $db_pwd = $_ENV['DB_PASSWORD'];
