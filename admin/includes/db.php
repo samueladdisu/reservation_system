a8fa8278abec825d270d652cea39a70f3fc81d5d
samuel addisu <samueladdisu7@gmail.com>
2022-08-06 10:11:27 +0300

    fix sign up page

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,13 +1,13 @@
 <?php
 
-include($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . basename(dirname(dirname(dirname(__FILE__)))) . "/admin_config.php");
-// include(dirname(dirname(dirname(__FILE__))) . "/admin_config.php");
+// include($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . basename(dirname(dirname(dirname(__FILE__)))) . "/admin_config.php");
+include(dirname(dirname(dirname(__FILE__))) . "/admin_config.php");
 $db_username = $_ENV['DB_USERNAME'];
 $db_pwd = $_ENV['DB_PASSWORD'];
 $db_name = $_ENV['DB_NAME'];
 
-// $connection = mysqli_connect('localhost', $db_username, $db_pwd, $db_name);
-$connection = mysqli_connect('localhost', 'root', '', 'lalibela');
+$connection = mysqli_connect('localhost', $db_username, $db_pwd, $db_name);
+// $connection = mysqli_connect('localhost', 'root', '', 'lalibela');
 
 if (!$connection) {
   echo "Error: Unable to connect to MySQL." . PHP_EOL;
