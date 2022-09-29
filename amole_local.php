d1740a77e5d40baebe44ef33c3c00f8687bc7668
Samuel Addisu <samueladdisu7@gmail.com>
2022-09-29 09:08:55 +0300

    amole test3

--- amole_local.php
+++ amole_local.php
@@ -11,7 +11,7 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => 'BODY_CardNumber=0911091185&BODY_ExpirationDate=&BODY_PIN=9999&BODY_PaymentAction=01&BODY_AmountX=200&BODY_AmoleMerchantID=BOSTONP&BODY_OrderDescription=YourTransDescription&BODY_SourceTransID=YourUniqueTransID&BODY_VendorAccount=&BODY_AdditionalInfo1=&BODY_AdditionalInfo2=&BODY_AdditionalInfo3=&BODY_AdditionalInfo4=&BODY_AdditionalInfo5=',
+  CURLOPT_POSTFIELDS => 'BODY_CardNumber=5700021635629825&BODY_ExpirationDate=0825&BODY_PIN=3106&BODY_PaymentAction=01&BODY_AmountX=200&BODY_AmoleMerchantID=BOSTONP&BODY_OrderDescription=YourTransDescription&BODY_SourceTransID=YourUniqueTransID&BODY_VendorAccount=&BODY_AdditionalInfo1=&BODY_AdditionalInfo2=&BODY_AdditionalInfo3=&BODY_AdditionalInfo4=&BODY_AdditionalInfo5=',
   CURLOPT_HTTPHEADER => array(
     'HDR_Signature: 9zqVsmJkYwBIbaJ7NdA-asfyoP0jiU71BbxRo_ALwiWq4vKa5Jnou4-fgTGBpy9YbrKbEt',
     'HDR_IPAddress: 109.70.148.58',
@@ -25,3 +25,4 @@ $response = curl_exec($curl);
 
 curl_close($curl);
 echo $response;
+
