df40c3a606c114decead00a6032dbb30bd54ea47
Samuel Addisu <samueladdisu7@gmail.com>
2022-09-30 18:48:55 +0300

    check callback

--- chapa.php
+++ chapa.php
@@ -94,7 +94,7 @@
       'email' => $_SESSION['email'],
       'first_name' => $_SESSION['fName'],
       'last_name' => $_SESSION['lName'],
-      'tx_ref' =>  9885,
+      'tx_ref' =>  986585,
       'callback_url' => 'https://test.kurifturesorts.com/chapaCompleted/',
       'return_url' => 'https://test.kurifturesorts.com/'
     ),
