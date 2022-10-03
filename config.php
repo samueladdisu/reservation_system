93eca365906c1a55cb5d355f738bbb6dfcb74627
Samuel Addisu <samueladdisu7@gmail.com>
2022-10-03 15:57:45 +0300

    sendgrid_test1

--- config.php
+++ config.php
@@ -8,7 +8,3 @@ require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-use Mailgun\Mailgun;
-// First, instantiate the SDK with your API credentials
-$mg = Mailgun::create($_ENV['MAILGUN_API_KEY']);
