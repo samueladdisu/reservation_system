0292fc7e0dbace5f368289dc2f67837ece42dd8d
samuel addisu <samueladdisu7@gmail.com>
2022-10-11 11:09:44 +0300

    chapa live key1

--- chapa.php
+++ chapa.php
@@ -88,7 +88,8 @@
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => 'POST',
     CURLOPT_POSTFIELDS => array(
-      'amount' =>  $price,
+      'amount' =>  1,
+      // 'amount' =>  $price,
       // 'key' => $_ENV['CHAPA_PUB'],
       'currency' => $currency,
       'email' => $_SESSION['email'],
