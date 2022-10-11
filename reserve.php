b866926e223b32a3dc3be314723321a995137947
samuel addisu <samueladdisu7@gmail.com>
2022-10-11 10:41:00 +0300

    diff dates

--- reserve.php
+++ reserve.php
@@ -414,6 +414,43 @@ if (isset($_GET['checkout'])) {
       </div>
     </div>
     <div v-else>
+
+      <div class="form-wrapper">
+
+        <div class="modal" tabindex="-1" role="dialog" id="TimesUP">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Kuriftu</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>are you still there.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" @click="clearOrder">Cancel</button>
+                <button type="button" class="btn btn-red BAlert" data-dismiss="modal" @click="TimerExtend()">Yes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <form class="myform row" @submit.prevent="submitData">
+
+          <div class="form-group col-lg-6">
+            <label for="date" class="label-date" id="label">Select Date</label>
+            <input type="text" class="form-control" name="daterange" id="date" value="" readonly />
+          </div>
+          <div class="form-group col-lg-6">
+            <button type="submit" class="btn btn-black" id="check">
+              Check Availability
+            </button>
+          </div>
+        </form>
+      </div>
+      
       <div class="centerSAD">
         <div class="container-sm complete-cont  ">
           <div class="check-box-wrapper">
