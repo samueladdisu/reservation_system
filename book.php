3f08ba580eaa11183b0e59835ff36a7f6aa0afeb
samuel addisu <samueladdisu7@gmail.com>
2022-11-03 11:46:02 +0300

    query fix

--- book.php
+++ book.php
@@ -30,11 +30,11 @@ if ($received_data->action == 'filter') {
   NOT IN 
       ( SELECT b_roomId
         FROM booked_rooms 
-        WHERE '$checkin' > b_checkin AND '$checkin' < b_checkout 
+        WHERE '$checkin' >= b_checkin AND '$checkin' < b_checkout 
         UNION
         SELECT b_roomId
         FROM booked_rooms
-        WHERE '$checkout' > b_checkin AND '$checkout' < b_checkout
+        WHERE '$checkout' >= b_checkin AND '$checkout' < b_checkout
         )
   AND room_location = '$location' AND room_status <> 'Hold' ORDER BY room_acc";
 
