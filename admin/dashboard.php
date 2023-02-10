16b97fe2b04a27fe2052bcfdb58d88cd40afba49
samueladdisu <samueladdisu7@gmail.com>
2023-02-10 16:18:18 +0300

    home work

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -23,12 +23,12 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-900">Welcome to Admin
+          <div class="row mb-4">
+            <h1 class="h3 mb-0 col-8 text-gray-900">Welcome to Admin
               | <small class="text-gray-600"><?php echo $_SESSION['username']; ?></small>
             </h1>
 
-            <!-- 
+            <!--             
                         <div class="dropdown show">
                             <a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-download fa-sm text-white-50"></i>
@@ -42,7 +42,16 @@
                             </div>
                         </div> -->
 
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <select name="" id="" class="custom-select col-2">
+              <option value="">today</option>
+              <option value="">this week</option>
+              <option value="">this month</option>
+              <option value="">this year</option>
+            </select>
+
+            <div class="col-2 ">
+
+
 
               <button data-toggle="modal" data-target="#ReportModalCenter" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Sales Report</button>
             </div>
@@ -62,18 +71,20 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                          Reservations</div>
-
+                          Rooms</div>
+                          <div class='h6 mb-0 mt-2 text-gray-600'>
+                            100 Available/ 90 Booked
+                          </div>
                         <?php
                         $location = $_SESSION['user_location'];
-                        if ($location == "Boston") {
-                          $query = "SELECT * FROM reservations";
-                        } else {
-                          $query = "SELECT * FROM reservations WHERE res_location = '$location' ";
-                        }
-                        $select_all_posts = mysqli_query($connection, $query);
-                        $post_counts = mysqli_num_rows($select_all_posts);
-                        echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>$post_counts</div>";
+                        // if ($location == "Boston") {
+                        //   $query = "SELECT * FROM reservations";
+                        // } else {
+                        //   $query = "SELECT * FROM reservations WHERE res_location = '$location' ";
+                        // }
+                        // $select_all_posts = mysqli_query($connection, $query);
+                        // $post_counts = mysqli_num_rows($select_all_posts);
+                        // echo "";
 
                         ?>
 
@@ -96,19 +107,22 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                          Rooms</div>
+                          Arrival & departure</div>
+                          <div class='h6 mb-0 mt-2 text-gray-600'>
+                           80 Arrival/ 70 Departure
+                        </div>
                         <?php
 
-                        if ($location == 'Boston') {
+                        // if ($location == 'Boston') {
 
-                          $query = "SELECT * FROM rooms";
-                        } else {
+                        //   $query = "SELECT * FROM rooms";
+                        // } else {
 
-                          $query = "SELECT * FROM rooms WHERE room_location = '$location'";
-                        }
-                        $select_all_categories = mysqli_query($connection, $query);
-                        $category_counts = mysqli_num_rows($select_all_categories);
-                        echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>$category_counts</div>";
+                        //   $query = "SELECT * FROM rooms WHERE room_location = '$location'";
+                        // }
+                        // $select_all_categories = mysqli_query($connection, $query);
+                        // $category_counts = mysqli_num_rows($select_all_categories);
+                        // echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>$category_counts</div>";
 
                         ?>
                       </div>
@@ -128,14 +142,15 @@
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Users
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">New Bookings
                         </div>
+                        <div class='h6 mb-0 mt-2 text-gray-600'>12</div>
                         <?php
 
-                        $query = "SELECT * FROM users";
-                        $select_all_users = mysqli_query($connection, $query);
-                        $users_counts = mysqli_num_rows($select_all_users);
-                        echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>$users_counts</div>";
+                        // $query = "SELECT * FROM users";
+                        // $select_all_users = mysqli_query($connection, $query);
+                        // $users_counts = mysqli_num_rows($select_all_users);
+                        // echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>$users_counts</div>";
 
                         ?>
 
@@ -152,22 +167,23 @@
             <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
               <a href="./acc.php">
-                <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card border-left-info shadow h-100 py-2">
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                          Room Types</div>
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                          Special Requests</div>
+                          <div class='h6 mb-0 mt-2 text-gray-600'>13</div>
                         <?php
 
-                        if ($location == 'Boston') {
-                          $query = "SELECT * FROM room_type";
-                        } else {
-                          $query = "SELECT * FROM room_type WHERE type_location = '$location' ";
-                        }
-                        $select_all_comments = mysqli_query($connection, $query);
-                        $comments_counts = mysqli_num_rows($select_all_comments);
-                        echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>$comments_counts</div>";
+                        // if ($location == 'Boston') {
+                        //   $query = "SELECT * FROM room_type";
+                        // } else {
+                        //   $query = "SELECT * FROM room_type WHERE type_location = '$location' ";
+                        // }
+                        // $select_all_comments = mysqli_query($connection, $query);
+                        // $comments_counts = mysqli_num_rows($select_all_comments);
+                        // echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>$comments_counts</div>";
 
                         ?>
                       </div>
